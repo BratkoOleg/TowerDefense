b12002521303e7b32b3e229b1cb01f6c8ecf8372
BratkoOleg <124066818+BratkoOleg@users.noreply.github.com>
2023-09-18 18:08:51 +0400

    added pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private Button _buttonPause;
+    [SerializeField] private Button _buttonExit;
+    [SerializeField] private Button _buttonContinue;
+    [SerializeField] private GameObject _pauseMenu;
+
+    void OnEnable()
+    {
+        _buttonPause.onClick.AddListener(SetMenuOpen);
+        _buttonContinue.onClick.AddListener(Continue);
+        _buttonExit.onClick.AddListener(ExitFromApp);
+    }
+
+    void OnDisable()
+    {
+        _buttonPause.onClick.RemoveAllListeners();
+        _buttonContinue.onClick.RemoveAllListeners();
+        _buttonExit.onClick.RemoveAllListeners();
+    }
+
+    private void SetMenuOpen()
+    {
+        _pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    private void ExitFromApp()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void Continue()
+    {
+        _pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
