065e1e24503d59f5dabf6a8fddb0d84b42fb7c11
BratkoOleg <124066818+BratkoOleg@users.noreply.github.com>
2023-09-26 02:53:00 +0400

    upgraded menu sup tower

--- Assets/Scripts/TowerS/UpgradeSupTower.cs
+++ Assets/Scripts/TowerS/UpgradeSupTower.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UpgradeSupTower : MonoBehaviour
 {
-    [SerializeField] Button _openMenu;
-    [SerializeField] Button _upgrade;
-    [SerializeField] Button _destroy;
-    [SerializeField] GameObject _menu;
+    [SerializeField] private Button _openMenu;
+    [SerializeField] private Button _upgrade;
+    [SerializeField] private Button _destroy;
+    [SerializeField] private Button _exit;
+    [SerializeField] private GameObject _menu;
+    [SerializeField] int _costUpgrade;
+    [SerializeField] private TextMeshProUGUI _cost;
 
     void OnEnable()
     {
+        SetText(_costUpgrade);
         _openMenu.onClick.AddListener(OpenMenu);
         _upgrade.onClick.AddListener(Upgrade);
         _destroy.onClick.AddListener(Destroy);
+        _exit.onClick.AddListener(CloseMenu);
     }
 
     void OnDisable()
@@ -22,6 +28,12 @@ public class UpgradeSupTower : MonoBehaviour
         _openMenu.onClick.RemoveAllListeners();
         _upgrade.onClick.RemoveAllListeners();
         _destroy.onClick.RemoveAllListeners();
+        _exit.onClick.RemoveAllListeners();
+    }
+
+    private void CloseMenu()
+    {
+        _menu.SetActive(false);
     }
 
     private void OpenMenu()
@@ -31,12 +43,25 @@ public class UpgradeSupTower : MonoBehaviour
 
     private void Upgrade()
     {
-        Debug.Log("tower was upgraded");
-        _menu.SetActive(false);
+        if(Wallet._coinsAmount >= _costUpgrade)
+        {
+            Wallet._coinsAmount -= _costUpgrade;
+
+            _costUpgrade += (int)(_costUpgrade * 0.5f);
+            SetText(_costUpgrade);
+
+            Debug.Log("tower was upgraded");
+            _menu.SetActive(false);
+        }
     }
 
     private void Destroy()
     {
         Destroy(gameObject);
+    }
+
+    private void SetText(int coins)
+    {
+        _cost.text = "" + coins;
     }
 }
