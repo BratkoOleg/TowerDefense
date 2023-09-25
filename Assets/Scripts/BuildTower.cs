f38d5c6308adb9d75dc13c4fc3a45f792281abc8
BratkoOleg <124066818+BratkoOleg@users.noreply.github.com>
2023-09-25 18:11:14 +0400

    added build system with check the near of tower

--- Assets/Scripts/BuildTower.cs
+++ Assets/Scripts/BuildTower.cs
@@ -10,6 +10,7 @@ public class BuildTower : MonoBehaviour
     [SerializeField] private GameObject _tower;
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private Tile _towerTile;
+    [SerializeField] private Tile _buildEnable;
     [SerializeField] private GameObject _grid;
     private bool _buildingMode = false;
 
@@ -29,10 +30,6 @@ public class BuildTower : MonoBehaviour
         {
             _buildingMode = true;
         }
-        else
-        {
-            _buildingMode = false;
-        }
     }
 
     void Update()
@@ -51,7 +48,7 @@ public class BuildTower : MonoBehaviour
     private void SetBuild()
     {
         Vector3Int coordinate = _tilemap.WorldToCell(GetMousePosition());
-        if(_tilemap.GetTile(coordinate) != _towerTile)
+        if(_tilemap.GetTile(coordinate) == _buildEnable)
         {
             _tilemap.SetTile(coordinate, _towerTile);
             Instantiate(_tower, coordinate, Quaternion.identity);
---
+++ Assets/Scripts/SetTilesAroundTower.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SetTilesAroundTower : MonoBehaviour
+{
+    [SerializeField] Tilemap _tilemap;
+    [SerializeField] Tile _towerTile;
+
+    [SerializeField] Tile _buildEnable;
+    private Vector3Int[] _directions =  {Vector3Int.up, Vector3Int.down, Vector3Int.left, Vector3Int.right};
+
+    void Awake()
+    {
+        _tilemap = GameObject.FindGameObjectWithTag("Tilemap").GetComponent<Tilemap>();
+        SetTilesAround();
+    }
+
+    private void SetTilesAround()
+    {
+        for(int i = 0; i < _directions.Length; i++)
+        {
+            if(_tilemap.GetTile(_directions[i] + SetPosiotion()) != _towerTile)
+            _tilemap.SetTile(_directions[i] + SetPosiotion(), _buildEnable);
+        }
+        Debug.Log(gameObject.name + " set around");
+    }
+
+    private Vector3Int SetPosiotion()
+    {
+        Vector3Int pos = _tilemap.WorldToCell(transform.position);
+        return pos;
+    }
+}
