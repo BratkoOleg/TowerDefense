97c4841c79c92f746e871addc955996d355e9ab3
BratkoOleg <124066818+BratkoOleg@users.noreply.github.com>
2023-10-04 03:43:09 +0400

    added limit of builded towers

--- Assets/Scripts/TowerS/BuildTower.cs
+++ Assets/Scripts/TowerS/BuildTower.cs
@@ -14,11 +14,15 @@ public class BuildTower : MonoBehaviour
     [SerializeField] private Transform _parent;
     [SerializeField] private int _buildCost;
     [SerializeField] private TextMeshProUGUI _costText;
+    [SerializeField] private TextMeshProUGUI _amountBuildedTowers;
+    private int _buildsEnable = 0;
+    private int lvl;
     public static int _amountTowers;
     public static bool _buildingMode = false;
 
     void OnEnable()
     {
+        EventBus.Instance.LeveledUp += LeveledUp;
         _button.onClick.AddListener(SetMode);
         _costText.text = "" + _buildCost;
     }
@@ -28,9 +32,16 @@ public class BuildTower : MonoBehaviour
         _button.onClick.RemoveAllListeners();
     }
 
+    private void LeveledUp(int value)
+    {
+        lvl = value;
+        _buildsEnable++;
+        _amountBuildedTowers.text = _amountTowers + "/" + lvl;
+    }
+
     private void SetMode()
     {
-        if(_buildingMode == false)
+        if(_buildingMode == false && _buildsEnable > 0 && Wallet._coinsAmount >= _buildCost)
         {
             _buildingMode = true;
             Time.timeScale = 0;
@@ -58,12 +69,14 @@ public class BuildTower : MonoBehaviour
     private void SetBuild()
     {
         Vector3Int coordinate = _tilemap.WorldToCell(GetMousePosition());
-        if(_tilemap.GetTile(coordinate) == _buildEnable && Wallet._coinsAmount >= _buildCost)
+        if(_tilemap.GetTile(coordinate) == _buildEnable && Wallet._coinsAmount >= _buildCost && _buildsEnable > 0)
         {
             Wallet._coinsAmount -= _buildCost;
             _amountTowers++;
+            _buildsEnable--;
             _buildCost += _amountTowers * _buildCost;
             _costText.text = "" + _buildCost;
+            _amountBuildedTowers.text = _amountTowers + "/" + lvl;
             _tilemap.SetTile(coordinate, _towerTile);
             GameObject tower = Instantiate(_tower, coordinate, Quaternion.identity);
             tower.transform.SetParent(_parent);
