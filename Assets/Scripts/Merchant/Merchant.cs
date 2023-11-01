5153c11c00205531ad5db5d2503732d84353e2b5
BratkoOleg <124066818+BratkoOleg@users.noreply.github.com>
2023-11-01 16:44:35 +0400

    added system of available of merhcant shop

--- Assets/Scripts/Merchant/Merchant.cs
+++ Assets/Scripts/Merchant/Merchant.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Merchant : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float _time;
+    [SerializeField] private float _timer;
+    [SerializeField] private GameObject _window;
+    [SerializeField] private Button _exit;
+
+    void OnEnable()
     {
-        
+        _exit.onClick.AddListener(CloseShop);
     }
 
-    // Update is called once per frame
+    void OnDisable()
+    {
+        _exit.onClick.RemoveAllListeners();
+    }
+
     void Update()
     {
-        
+        _time += Time.deltaTime;
+
+        if(_time >= _timer)
+        OpenShop();
+    }
+
+    private void OpenShop()
+    {
+        if(_window.activeSelf == false)
+        {
+            Time.timeScale = 0;
+            _time = 0;
+            _window.SetActive(true);
+        }
+    }
+
+    private void CloseShop()
+    {
+        Time.timeScale = 1;
+        _window.SetActive(false);   
     }
 }
